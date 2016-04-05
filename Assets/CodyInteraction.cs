8d0ec62f73950329d4b28cd612c86df4d6965d7f
gimoz71 <gm3871@gmail.com>
2016-04-05 15:25:43 +0200

    primo commit

---
+++ Assets/CodyInteraction.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using System.Collections;
+
+
+//---------------------------------------------------
+// Script interazione Einstein
+//---------------------------------------------------
+
+public class EinsteinInteraction : MonoBehaviour {
+
+	// dichiarazioni variabili ed oggetti
+
+	private Animator animator;
+	public Transform targetCody;
+	public Transform targetSerena;
+	public AudioClip EinsteinIdle;
+	public AudioClip EinsteinToCody;
+	public AudioClip EinsteinToSerena;
+
+
+
+	//---------------------------------------------------
+	// settaggi all'avvio
+	//---------------------------------------------------
+
+	void Start(){
+
+		// mi assicuro che tutte le variabil di interazione siano a zero (idle state in )
+		animator = GetComponent <Animator> ();
+		animator.SetBool ("lookCody", false);
+		animator.SetBool ("lookSerena", false);
+		//GetComponent<AudioSource>().PlayOneShot(CodyIdle);
+	}
+
+
+
+	//---------------------------------------------------
+	// inizio interazione audio (play one time)
+	//---------------------------------------------------
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.name == "cody") {
+			GetComponent<AudioSource>().PlayOneShot(EinsteinToCody);
+		}
+		if (other.gameObject.name == "serena") {
+			GetComponent<AudioSource>().PlayOneShot(EinsteinToSerena);
+		}
+	}
+
+
+	//---------------------------------------------------
+	// inizio interazione
+	//---------------------------------------------------
+	void OnTriggerStay (Collider other) {
+
+
+		//-------------------------------
+		// interazione Einstein <-> Cody
+		//-------------------------------
+
+		if (other.gameObject.name == "cody") {
+
+			// gurda Cody
+			transform.LookAt(targetCody); 
+
+			// script di fix assi di rotazione 
+			var rot = transform.localRotation.eulerAngles;
+			rot.z = 0f;
+			rot.x = 0f;
+			transform.localRotation = Quaternion.Euler(rot);
+
+			// attivo animazione per Einstein verso Cody
+			animator.SetBool ("lookCody", true);
+		}
+
+
+		//-------------------------------
+		// interazione Einstein <-> Serena 
+		//-------------------------------
+
+		if (other.gameObject.name == "Serena") {
+
+			// gurda Serena
+			transform.LookAt(targetSerena); 
+
+			// script di fix assi di rotazione 
+			var rot = transform.localRotation.eulerAngles;
+			rot.z = 0f;
+			rot.x = 0f;
+			transform.localRotation = Quaternion.Euler(rot);
+
+			// attivo animazione per Einstein verso Serena
+			animator.SetBool ("lookSerena", true);
+		}
+
+	}
+
+
+	//---------------------------------------------------
+	// fine interazione
+	//---------------------------------------------------
+
+	void OnTriggerExit (Collider other) {
+		Debug.Log ("Uscito");
+
+		// resetto tutte le trasformazioni
+		transform.localRotation = Quaternion.identity;
+		transform.localPosition = Vector3.zero;
+		transform.localScale = Vector3.one;
+
+		// setto tutte le variabili di inerazione a zero
+		animator.SetBool ("lookCody", false);
+		animator.SetBool ("lookSerena", false);
+
+		// rilancio audio no interazione
+		GetComponent<AudioSource>().PlayOneShot(EinsteinIdle);
+	}
+}
---
+++ Assets/EinsteinInteraction.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using System.Collections;
+
+
+//---------------------------------------------------
+// Script interazione Cody Simpson
+//---------------------------------------------------
+
+public class CodyInteraction : MonoBehaviour {
+
+	// dichiarazioni variabili ed oggetti
+
+	private Animator animator;
+	public Transform targetEinstein;
+	public Transform targetSerena;
+	public AudioClip CodyIdle;
+	public AudioClip CodyToEinstein;
+	public AudioClip CodyToSerena;
+
+
+
+	//---------------------------------------------------
+	// settaggi all'avvio
+	//---------------------------------------------------
+
+	void Start(){
+
+		// mi assicuro che tutte le variabil di interazione siano a zero (idle state in )
+		animator = GetComponent <Animator> ();
+		animator.SetBool ("lookEinstein", false);
+		animator.SetBool ("lookSerena", false);
+		//GetComponent<AudioSource>().PlayOneShot(CodyIdle);
+	}
+
+
+
+	//---------------------------------------------------
+	// inizio interazione audio (play one time)
+	//---------------------------------------------------
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.name == "einstein") {
+			GetComponent<AudioSource>().PlayOneShot(CodyToEinstein);
+		}
+		if (other.gameObject.name == "serena") {
+			GetComponent<AudioSource>().PlayOneShot(CodyToSerena);
+		}
+	}
+
+
+	//---------------------------------------------------
+	// inizio interazione
+	//---------------------------------------------------
+	void OnTriggerStay (Collider other) {
+
+
+		//-------------------------------
+		// interazione Cody <-> Einstein
+		//-------------------------------
+
+		if (other.gameObject.name == "einstein") {
+
+			// gurda Einstein
+			transform.LookAt(targetEinstein); 
+
+			// script di fix assi di rotazione 
+			var rot = transform.localRotation.eulerAngles;
+			rot.z = 0f;
+			rot.x = 0f;
+			transform.localRotation = Quaternion.Euler(rot);
+
+			// attivo animazione per Cody verso Einstein
+			animator.SetBool ("lookEinstein", true);
+		}
+
+
+		//-------------------------------
+		// interazione Cody <-> Serena 
+		//-------------------------------
+
+		if (other.gameObject.name == "Serena") {
+
+			// gurda Serena
+			transform.LookAt(targetSerena); 
+
+			// script di fix assi di rotazione 
+			var rot = transform.localRotation.eulerAngles;
+			rot.z = 0f;
+			rot.x = 0f;
+			transform.localRotation = Quaternion.Euler(rot);
+
+			// attivo animazione per Cody verso Serena
+			animator.SetBool ("lookSerena", true);
+		}
+
+	}
+
+
+	//---------------------------------------------------
+	// fine interazione
+	//---------------------------------------------------
+
+	void OnTriggerExit (Collider other) {
+		Debug.Log ("Uscito");
+
+		// resetto tutte le trasformazioni
+		transform.localRotation = Quaternion.identity;
+		transform.localPosition = Vector3.zero;
+		transform.localScale = Vector3.one;
+
+		// setto tutte le variabili di inerazione a zero
+		animator.SetBool ("lookEinstein", false);
+		animator.SetBool ("lookSerena", false);
+
+		// rilancio audio no interazione
+		GetComponent<AudioSource>().PlayOneShot(CodyIdle);
+	}
+}
